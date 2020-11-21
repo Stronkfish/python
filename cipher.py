46fa2195ee9f0fcdd8df16704a60193ae53fce38
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-21 14:29:35 +1100

    Update cipher.py

--- cipher.py
+++ cipher.py
@@ -1,9 +1,12 @@
 print("Stronkfish Brand Caeser Cipher Tool")
+# Cipher Range
 L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
 I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+# Cipher Spec
 cry = int(input("Encrypt or Decrypt? (1/2) "))
-plaintext = input("Enter message you would like to Encrypt/Decrypt: ")
+plaintext = input("Enter the Message you would like to Encrypt/Decrypt: ")
 key = int(input("What would you like the Key to be? "))
+# Cipher
 ciphertext = ""
 plaintext2 = ""
 if cry == 2:
@@ -18,7 +21,5 @@ for c in ciphertext.upper():
         plaintext2 += I2L[ (L2I[c] - key)%26 ]
     else:
         plaintext2 += c
-if cry == 1:
-    print(f"Original message: {plaintext} \n Encrypted Message: {ciphertext} \n Key: {key}")
-if cry == 2:
-    print(f"Original message: {plaintext} \n Decrypted Message: {ciphertext} \n Key: {key}")
+# Cipher & Settings
+print(f"Original Message: {plaintext} \n Ciphered Message: {ciphertext} \n Key: {key}")
