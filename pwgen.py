e67d89b1396dfb117bbd1e20c11e4c00d3cc4128
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-22 14:15:59 +1100

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -8,10 +8,22 @@ psORu = input("Would you like to use your own Set of Characters? (Y/N) ")
 if psORu == "Y":
     c2 = input("Which Characters would you like to use? ")
 if psORu == "N":
-    c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
+    c3s = int(input("Please choose from a selection of character sets. \n1 = All characters \n2 = Alphanumeric Characters \n3 = Numbers & Symbols \nChar Set: "))
+    if c3s == 1:
+        c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
+    if c3s == 2:
+        c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    if c3s == 3:
+        c2 = "1234567890/*-!@#$%^&*()_+[];',./{}|:<>?"
 # Gen
 pw = ""
 for c in range(pwlen):
     pw += random.choice(c2)
 # String & Settings
-print(f"String Generated \n Length: {pwlen} \n Character Set: {c2} \n Password: {pw}")
+print(f"String Generated \nLength: {pwlen} \nCharacter Set: {c2} \nPassword: {pw}")
+# pwlen = Password Length
+    # psORu = Preselected Character set or User Character set
+        # c = Characters
+            # c2 = Character set
+                # c3s Preselected Character Sets
+                    # pw = Password
