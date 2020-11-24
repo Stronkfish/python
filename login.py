f22521c8bc73ceb4f1392b3e24dad0ba02a039c1
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-24 14:44:48 +1100

    Update login.py - Fixed code like a mega boss

--- login.py
+++ login.py
@@ -2,10 +2,6 @@
 u3 = "sysadmin"
 pw3 = "boomer"
 e3 = "sysadmin@gmail.com"
-e1 = ""
-u2 = ""
-cpw1 = ""
-pw2 = ""
 # Create or Login Account
 main = input("create = Create Account \nlogin = Login an Existing Account \n")
 # Create, then Login
@@ -14,25 +10,29 @@ if main == "create":
     e1 = input("Email Address: ")
     pw1 = input("Password: ")
     cpw1 = input("Confirm Password: ")
-    if cpw1 != pw1:
-        print("Password and Confirm Password Mismatched.")
     if cpw1 == pw1:
         print("\nAccount successfully created!\n")
-        main2 = input("login = Login to Your New Account \nclose = Close this Window \n\n> ")
+        main2 = input("login = Login to Your New Account \nclose = Close this Window \n\n")
         if main2 == "login":
             u2 = input("Username/Email Address: ")
             pw2 = input("Password: ")
-    if main2 == "close":
-        print("Window Closed.")
-# Login Details Checking
-if main == "create":
-    if u2 == u1 or u3 or e1 or e3:
-        if pw2 == pw1 or pw3:
-            print(f"Logged in Successfully as {u2}!")
-        else:
-            print("Username, Email Address or Password were not recognised.")
-    else:
-        print("Username, Email Address or Password were not recognised.")
+            # Login Details Checking
+            if u1 == u2:
+                if pw1 == pw2:
+                    print(f"Logged in Successfully as {u2}!")
+            if e1 == u2:
+                if pw1 == pw2:
+                    print(f"Logged in Successfully as {u2}!")
+            if u3 == u2:
+                if pw3 == pw2:
+                    print(f"Logged in Successfully as {u2}!")
+            if e3 == u2:
+                if pw3 == pw2:
+                    print(f"Logged in Successfully as {u2}!")
+        if main2 == "close":
+            print("Window Closed.")   
+    if cpw1 != pw1:
+        print("Password and Confirm Password Mismatched.")
 # Login without Creating
 if main == "login":
     u2 = input("Username/Email Address: ")
@@ -40,7 +40,3 @@ if main == "login":
     if u2 == u3 or e3:
         if pw2 == pw3:
             print(f"Logged in Successfully as {u3}!")
-        else:
-            print("Username, Email Address or Password were not recognised.")
-    else:
-        print("Username, Email Address or Password were not recognised.")
