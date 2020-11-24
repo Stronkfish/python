28b33d8bc6e4410e8e3c5cfba028705173163f7e
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-24 14:51:51 +1100

    Update cipher.py
    -removed useless variable
    -minor changes elsewhere

--- cipher.py
+++ cipher.py
@@ -1,20 +1,19 @@
 print("Stronkfish Brand Caeser Cipher Tool")
-# Cipher Range
+# Letters to Integers / Inverse 
 L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
 I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
 # Cipher Spec
 cry = int(input("Encrypt or Decrypt? (1/2) "))
-plaintext = input("Enter the Message you would like to Encrypt/Decrypt: ")
+pltxt = input("Enter the Message you would like to Encrypt/Decrypt: ")
 key = int(input("What would you like the Key to be? "))
 # Cipher
-ciphertext = ""
-plaintext2 = ""
+citxt = ""
 if cry == 2:
     key = -key
-for c in plaintext.upper():
+for c in pltxt.upper():
     if c.isalpha():
-        ciphertext += I2L[ (L2I[c] + key)%26 ]
+        citxt += I2L[ (L2I[c] + key)%26 ]
     else:
-        ciphertext += c
+        citxt += c
 # Cipher & Settings
-print(f"Original Message: {plaintext} \nCiphered Message: {ciphertext} \nKey: {key}")
+print(f"Original Message: {pltxt} \nCiphered Message: {citxt} \nKey: {key}")
