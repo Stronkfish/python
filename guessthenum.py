37d16f2d19995b56217b2d00cbe097ed8dad9ad6
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-30 19:58:52 +1100

    Update guessthenum.py

--- guessthenum.py
+++ guessthenum.py
@@ -1,8 +1,20 @@
 import random
-num = (random.randint(1,100))
-attempts = 0
-print(num)
-guess = int(input("Stronkfish Guess the Number Game\n\nYou have 5 Attempts\n\nGuess the Number: "))
+att = 0
+num1 = int(input("Stronkfish Brand Guess the Number Game\n\nMinimum Number: "))
+num2 = int(input("Maximum Number: "))
+attw = int(input("Number of Attempts Allowed: "))
+num = random.randint(num1, num2)
+print(f"I am thinking of a number between {num1} and {num2}. You have {attw} Attempts to Guess it.")
+while att < attw:
+    guess = int(input("\nGuess the number: "))
+    att = att + 1
+    if guess < num:
+        print(f"\nToo Low!\nAttempts Remaining: {attw - att}")
+    if guess > num:
+        print(f"\nToo High!\nAttempts Remaining: {attw - att}")
+    if guess == num:
+        break
 if guess == num:
-    print("\nCorrect!")
-# placeholder
+    print(f"\nCorrect!\nTotal Attempts: {att}")
+if guess != num:
+    print(f"\nZero Attempts Remaining!\nThe Number I was thinking of is {num}")
