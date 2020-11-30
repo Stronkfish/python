60e52506168479ac39c2008a6477a06ac0b85bfc
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-30 14:56:24 +1100

    Create guessthenum.py

---
+++ guessthenum.py
@@ -0,0 +1,8 @@
+import random
+num = (random.randint(1,100))
+attempts = 0
+print(num)
+guess = int(input("Stronkfish Guess the Number Game\n\nYou have 5 Attempts\n\nGuess the Number: "))
+if guess == num:
+    print("\nCorrect!")
+# placeholder
