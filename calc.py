faf720c3998f5f60f807ec7735902526004258eb
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-30 14:18:54 +1100

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,10 @@
+main = input("Calculator\n\nAddition = a\nSubtraction = b\nMultiplication = c\nDivision = d\n\n")
+if main == "a":
+    print("\nYou have chosen Addition.\n\n")
+if main == "b":
+    print("cool")
+if main == "c":
+    print("cheese")
+if main == "d":
+    print("beans")
+# placeholder
