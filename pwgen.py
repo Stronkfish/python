0b37f41840f548cc716269945207f632722f5295
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-11-30 14:40:04 +1100

    Update pwgen.py

--- pwgen.py
+++ pwgen.py
@@ -1,23 +1,17 @@
-print("Stronkfish Brand Password Generator")
-# Random Gen Plugin
-import random
-# Preset Spec
-pwlen = int(input("How long do you want your Password to be? "))
+import random # Random Gen Plugin (1)
+pwlen = int(input("Stronkfish Brand Password Generator\n\nHow long do you want your Password to be? ")) # Preset Spec (2-3)
 psORu = input("Would you like to use your own Set of Characters? (Y/N) ")
-# Char Sets
-if psORu == "Y":
+if psORu == "Y": # Char Sets (4-13)
     c2 = input("Which Characters would you like to use? ")
 if psORu == "N":
-    c3s = int(input("Please choose from a selection of character sets. \n1 = All characters \n2 = Alphanumeric Characters \n3 = Numbers & Symbols \nChar Set: "))
+    c3s = int(input("Please choose from a selection of character sets. \nAll Characters = 1 \nAlphanumeric Characters = 2 \nNumbers & Symbols = 3 \nCharacter Set: "))
     if c3s == 1:
         c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
     if c3s == 2:
         c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
     if c3s == 3:
         c2 = "1234567890/*-!@#$%^&*()_+[];',./{}|:<>?"
-# Gen
-pw = ""
+pw = "" # Gen (14-16)
 for c in range(pwlen):
     pw += random.choice(c2)
-# String & Settings
-print(f"String Generated \nLength: {pwlen} \nCharacter Set: {c2} \nPassword: {pw}")
+print(f"String Generated \nLength: {pwlen} \nCharacter Set: {c2} \nPassword: {pw}") # String & Settings (17)
