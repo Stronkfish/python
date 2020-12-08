372cfd88560f7c4fcd61801e1065479a52abc904
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-12-08 20:00:57 +1100

    Delete tts.py

--- TTS/tts.py
+++
@@ -1,7 +0,0 @@
-from gtts import gTTS
-lan = 'en'
-main = input("Stronkfish Brand TTS\n\nText: ")
-fn = input("File name: ")
-speech = gTTS(text=main, lang=lan, slow=False) 
-speech.save(f"{fn}.mp3")
-print("Audio Saved.")
