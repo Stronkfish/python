a3ded1e318ad928834c481be365ea61780db087a
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-12-12 11:15:50 +1100

    Update tts.py

--- Text to Speech Tool/tts.py
+++ Text to Speech Tool/tts.py
@@ -1,7 +1,7 @@
-from gtts import gTTS
-lan = 'en'
-main = input("Stronkfish Brand TTS\n\nText: ")
+from gtts import gTTS # Google TTS Plugin (1)
+lan = 'en' # Language (2)
+main = input("Stronkfish Brand TTS\n\nText: ") # User Input (3-4)
 fn = input("File name: ")
-speech = gTTS(text=main, lang=lan, slow=False) 
+speech = gTTS(text=main, lang=lan, slow=False) # TTS File Create & Save (5-7)
 speech.save(f"{fn}.mp3")
 print("Audio Saved.")
