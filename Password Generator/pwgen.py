8f3b8fa999e3dff8ec76eff74c3d5b21e8242efb
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2020-12-13 13:37:34 +1100

    Update pwgen.py
    Changed variable names

--- Password Generator/pwgen.py
+++ Password Generator/pwgen.py
@@ -1,17 +1,17 @@
 import random # Random Gen Plugin (1)
-pwlen = int(input("Stronkfish Brand Password Generator\n\nHow long do you want your Password to be? ")) # Preset Spec (2-3)
-psORu = input("Would you like to use your own Set of Characters? (Y/N) ")
-if psORu == "Y": # Char Sets (4-13)
-    c2 = input("Which Characters would you like to use? ")
-if psORu == "N":
-    c3s = int(input("Please choose from a selection of character sets. \nAll Characters = 1 \nAlphanumeric Characters = 2 \nNumbers & Symbols = 3 \nCharacter Set: "))
-    if c3s == 1:
-        c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
-    if c3s == 2:
-        c2 = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    if c3s == 3:
-        c2 = "1234567890/*-!@#$%^&*()_+[];',./{}|:<>?"
-pw = "" # Gen (14-16)
-for c in range(pwlen):
-    pw += random.choice(c2)
-print(f"String Generated \nLength: {pwlen} \nCharacter Set: {c2} \nPassword: {pw}") # String & Settings (17)
+passlength = int(input("Stronkfish Brand Password Generator\n\nHow long do you want your Password to be? ")) # Preset Spec (2-3)
+selection = input("Would you like to use your own Set of Characters? (Y/N) ")
+if selection == "Y": # Char Sets (4-13)
+    chars = input("Which Characters would you like to use? ")
+if selection == "N":
+    charset = int(input("Please choose from a selection of character sets. \nAll Characters = 1 \nAlphanumeric Characters = 2 \nNumbers & Symbols = 3 \nCharacter Set: "))
+    if charset == 1:
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
+    if charset == 2:
+        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+    if charset == 3:
+        chars = "1234567890/*-!@#$%^&*()_+[];',./{}|:<>?"
+passw = "" # Gen (14-16)
+for c in range(passlength):
+    passw += random.choice(chars)
+print(f"String Generated \nLength: {passlength} \nCharacter Set: {chars} \nPassword: {passw}") # String & Settings (17)
