719038c0e4f31518e747bb522ee765d225d1cf3a
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2021-03-30 14:15:04 +1100

    Rewrite of Commenting

--- Guess the Number Game/guessthenum.py
+++ Guess the Number Game/guessthenum.py
@@ -1,20 +1,49 @@
-import random # Random Gen Plugin (1)
-att = 0 # Attempts Taken (2)
-num1 = int(input("Stronkfish Brand Guess the Number Game\n\nMinimum Number: ")) # Spec (3-5)
-num2 = int(input("Maximum Number: "))
-attw = int(input("Number of Attempts Allowed: "))
-num = random.randint(num1, num2) # Gen (6)
-print(f"I am thinking of a number between {num1} and {num2}. You have {attw} Attempts to Guess it.") # Number Range, Attempt Count Confirmation (7)
-while att < attw: # Number Guessing (8-16)
+# Random Integer Plugin.
+import random
+
+# Attempts Taken.
+att = 0
+
+# Minimum Number for the Game.
+minimum = int(input("Stronkfish Brand Guess the Number Game\n\nMinimum Number: ")) 
+
+# Maximum Number for the Game.
+maximum = int(input("Maximum Number: "))
+
+# Number of Attempts Allowed for the Game.
+att_allowed = int(input("Number of Attempts Allowed: "))
+
+# Generates the Number to Guess, within the Minimum and Maximum Number.
+num = random.randint(minimum, maximum)
+
+# Provides the Details for the Game - Minimum Number, Maximum Number, Attempts Allowed.
+print(f"I am thinking of a number between {minimum} and {maximum}. You have {att_allowed} Attempts to Guess it.")
+
+# Starts the Game. While the attempts counter is below the allowed attempts, the Game will continue until the Number is Guessed.
+while att < att_allowed:
+    
+    # User Input to Guess the Number.
     guess = int(input("\nGuess the number: "))
+    
+    # Adding to the Attempt Counter for each Guess.
     att += 1
+    
+    # The Guess is too Low, displays total Attempts Remaining.
     if guess < num:
-        print(f"\nToo Low!\nAttempts Remaining: {attw - att}")
+        print(f"\nToo Low!\nAttempts Remaining: {att_allowed - att}")
+        
+    # The Guess is too High, displays total Attempts Remaining.
     if guess > num:
-        print(f"\nToo High!\nAttempts Remaining: {attw - att}")
+        print(f"\nToo High!\nAttempts Remaining: {att_allowed - att}")
+        
+    # The Guess is Correct, breaks the Guess Loop.
     if guess == num:
         break
-if guess == num: # Correct Answer, Total Attempts used (17-18)
+        
+# Guessed the Number with the Total Attempts taken.
+if guess == num:
     print(f"\nCorrect!\nTotal Attempts: {att}")
-if guess != num: # Incorrect Answer, ran out of Attempts (19-20)
+    
+# Failed to Guess the Number, Zero Attempts Remaining, and the Correct Number.
+if guess != num:
     print(f"\nZero Attempts Remaining!\nThe Number I was thinking of is {num}")
