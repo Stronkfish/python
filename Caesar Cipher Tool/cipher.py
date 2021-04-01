0d90cd372a325cb0c1bb8d13597ad93277b951cf
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2021-04-01 19:37:55 +1100

    Update cipher.py

--- Caesar Cipher Tool/cipher.py
+++ Caesar Cipher Tool/cipher.py
@@ -1,14 +1,25 @@
-L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26))) # Letters to Integers / Inverse (1-2)
+ # Letters to Integers / Inverse
+L2I = dict(zip("ABCDEFGHIJKLMNOPQRSTUVWXYZ",range(26)))
 I2L = dict(zip(range(26),"ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
-cry = int(input("Stronkfish Brand Caesar Cipher Tool\n\nEncrypt or Decrypt? (1/2) ")) # Cipher Spec (3-5)
-pltxt = input("Enter the Message you would like to Encrypt/Decrypt: ")
+
+# Cipher Specification
+enc_or_dec = int(input("Stronkfish Brand Caesar Cipher Tool\n\nEncrypt or Decrypt? (1/2) ")) 
+
+plaintext = input("Enter the Message you would like to Encrypt/Decrypt: ")
+
 key = int(input("What would you like the Key to be? "))
-citxt = "" # Cipher (6-13)
-if cry == 2:
+
+# Inverses Key for Decryption
+if enc_or_dec == 2:
     key = -key
-for c in pltxt.upper():
+
+# Cipher
+cipheredtext = ""
+for c in plaintext.upper():
     if c.isalpha():
-        citxt += I2L[ (L2I[c] + key)%26 ]
+        cipheredtext += I2L[ (L2I[c] + key)%26 ]
     else:
-        citxt += c
-print(f"Original Message: {pltxt} \nCiphered Message: {citxt} \nKey: {key}") # Cipher & Settings (14)
+        cipheredtext += c
+
+# Result, Settings Chosen
+print(f"Original Message: {plaintext} \nCiphered Message: {cipheredtext} \nKey: {key}")
