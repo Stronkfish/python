f051b8323ecb9a29271a513cdbe2c7d3c965c21c
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2021-04-04 13:25:20 +1000

    Update fahrenheitcelsius.py

--- Fahrenheit Celsius Converter/fahrenheitcelsius.py
+++ Fahrenheit Celsius Converter/fahrenheitcelsius.py
@@ -1,8 +1,19 @@
-main = input("Stronkfish Brand Temperature Converter\n\nCLS = Convert to Celsius\nFHR = Convert to Fahrenheit\n\n") # Choose Conversion CLS or FHR (1)
-conv = "" # Pre-Define (2)
-if main.upper() == "CLS": # CLS to FHR Conversion (3-5)
+# Choose whether to convert from Celsius to Fahrenheit and vice versa
+main = input("Stronkfish Brand Temperature Converter\n\nCLS = Convert to Celsius\nFHR = Convert to Fahrenheit\n\n")
+
+# Pre-Defined Variable
+conv = ""
+
+# If the User is Converting to Fahrenheit
+if main.upper() == "FHR":
     conv = int(input("\nEnter the Temperature in Celsius to convert it into Fahrenheit: "))
+
+# Fahrenheit = Celsius x 9/5 + 32
     print(f"\nCelsius: {conv} \nFahrenheit: {conv*9/5+32}")
-if main.upper() == "FHR": # FHR to CLS Conversion (6-8)
+
+# If the User is Converting to Celsius
+if main.upper() == "CLS":
     conv = int(input("\nEnter the Temperature in Fahrenheit to convert it into Celsius: "))
+
+# Celsius = (Fahrenheit - 32) x 5/9
     print(f"\nFahrenheit: {conv} \nCelsius: {(conv-32)*5/9}")
