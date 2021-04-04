9835d6a3546a66e6c0aab2836d30b3882cd8808f
Stronkfish <55682124+Stronkfish@users.noreply.github.com>
2021-04-04 13:40:26 +1000

    Update pwgen.py

--- Password Generator/pwgen.py
+++ Password Generator/pwgen.py
@@ -1,17 +1,26 @@
-import random # Random Gen Plugin (1)
-passlength = int(input("Stronkfish Brand Password Generator\n\nHow long do you want your Password to be? ")) # Preset Spec (2-3)
+# Random Plugin.
+import random
+
+# Password Length.
+passlength = int(input("Stronkfish Brand Password Generator\n\nPassword Length: "))
+
+# Custom Character Set.
 selection = input("Would you like to use your own Set of Characters? (Y/N) ")
-if selection == "Y": # Char Sets (4-13)
-    chars = input("Which Characters would you like to use? ")
-if selection == "N":
-    charset = int(input("Please choose from a selection of character sets. \nAll Characters = 1 \nAlphanumeric Characters = 2 \nNumbers & Symbols = 3 \nCharacter Set: "))
-    if charset == 1:
-        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
-    if charset == 2:
-        chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
-    if charset == 3:
-        chars = "1234567890/*-!@#$%^&*()_+[];',./{}|:<>?"
-passw = "" # Gen (14-16)
+
+# Choose which Characters you want in your Password.
+if selection.upper() == "Y":
+    characters = input("Characters: ")
+
+# All Characters.
+if selection.upper() == "N":
+    characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ`1234567890-=~!@#$%^&*()_+[];,./{}|:<>?"
+
+# Pre-Defined Variable.
+password = ""
+
+# At the specified Length of the Password, Generate a Random String with the Character Set chosen.
 for c in range(passlength):
-    passw += random.choice(chars)
-print(f"String Generated \nLength: {passlength} \nCharacter Set: {chars} \nPassword: {passw}") # String & Settings (17)
+    password += random.choice(characters)
+
+# Generated String, Characters Used, and the Length.
+print(f"String Generated \nLength: {passlength} \nCharacters Used: {characters} \nPassword: {password}")
